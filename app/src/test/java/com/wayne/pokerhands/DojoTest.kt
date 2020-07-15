6a847537cd750fe17d268fd20bb28d1aba638681
Wayne Chen <wayne.swchen@gmail.com>
2020-07-15 17:32:04 +0800

    First commit.

---
+++ app/src/test/java/com/wayne/pokerhands/DojoTest.kt
@@ -0,0 +1,15 @@
+package com.wayne.pokerhands
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Created by Wayne Chen on 2020/7/15.
+ */
+class DojoTest {
+
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
